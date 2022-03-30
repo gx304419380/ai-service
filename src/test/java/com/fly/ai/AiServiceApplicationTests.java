e5c1fa51cb68c5f0403313f7a86d586378c87373
gx304419380 <304419380@qq.com>
2022-03-30 20:49:40 +0800

    [UPD]增加新的分支：linux

--- src/test/java/com/fly/ai/AiServiceApplicationTests.java
+++ src/test/java/com/fly/ai/AiServiceApplicationTests.java
@@ -3,11 +3,12 @@ package com.fly.ai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AiServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        String env = System.getenv("DJL_CACHE_DIR");
+        System.out.println(env);
     }
 
 }
